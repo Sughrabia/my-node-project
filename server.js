19b645b034fea5e6ca5a2ebc12f81a2567c1aa83
Sughrabia <sughrabia@gmail.com>
2024-10-31 09:57:27 +0500

    new

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.json()); 
 
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/glamgrabstore';
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
+const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/glamgrabstore';
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000 // Adjust timeout as needed
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err.message, err.stack));
 
 
 
