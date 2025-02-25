a131b0a593e50b73aed01b0919145f08a869cb23
Sughrabia <sughrabia@gmail.com>
2025-02-25 16:17:51 +0500

    ..

--- models/login.js
+++ models/login.js
@@ -9,4 +9,4 @@ const userSchema = new mongoose.Schema({
   isVerified: { type: Boolean, default: false },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema, 'users');
